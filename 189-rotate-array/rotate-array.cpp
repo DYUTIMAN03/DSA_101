ad90eb1e9cefdc7ed308bdb5c892ebfc1bcf0d70
Dyutiman Bharadwaj <148200116+DYUTIMAN03@users.noreply.github.com>
2026-08-06 12:40:34 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (95.18%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -10,3 +10,26 @@ public:
         reverse(nums.begin()+k, nums.end());
     }
 };
+
+
+/*
+class Solution {
+public:
+    void reverse(vector<int>& nums, int left, int right) {
+        while (left < right) {
+            swap(nums[left], nums[right]);
+            left++;
+            right--;
+        }
+    }
+
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+};
+*/
