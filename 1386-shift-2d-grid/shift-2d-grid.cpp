f1762e58fb901b75ae74039d513dd8026729d0da
Dyutiman Bharadwaj <148200116+DYUTIMAN03@users.noreply.github.com>
2026-08-06 20:53:05 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (36.52%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -24,3 +24,27 @@ public:
         return grid;
     }
 };
+
+
+/*
+// take extra space
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> temp(m,vector<int>(n,0)); // create m*n matrix with all value = 0
+        for(int row = 0;  row < m; row++){
+            for(int col = 0; col < n; col++){
+                // formula to find index of that new value in matrix
+                int new_val = ((row * n + col) + k) %  (m*n); // % m*n prevent overflow
+                int new_row = new_val / n; // to find row
+                int new_col = new_val % n; // to find coloumn
+                temp[new_row][new_col] = grid[row][col];
+            }
+        }
+        return temp;
+    }
+};
+*/
