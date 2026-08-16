49ba737dd229713d91ed1dd3af5fdf4a4c974b47
Dyutiman Bharadwaj <148200116+DYUTIMAN03@users.noreply.github.com>
2026-08-16 22:09:14 +0530

    Time: 8 ms (24.23%) | Memory: 76.5 MB (10.86%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,16 +1,34 @@
+// class Solution {
+// public:
+//     vector<int> replaceElements(vector<int>& nums) {
+
+//         int n = nums.size();
+
+//         int maxRight = -1;
+        
+//         for(int i=n-1; i>=0; i--){
+//             int current = nums[i];
+//             nums[i] = maxRight;
+//             maxRight = max(maxRight,current);
+//         }
+//         return nums;    
+//     }
+// };
+
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& nums) {
-
+        vector<int>ans;
         int n = nums.size();
 
         int maxRight = -1;
         
         for(int i=n-1; i>=0; i--){
             int current = nums[i];
-            nums[i] = maxRight;
+            ans.push_back(maxRight);
             maxRight = max(maxRight,current);
         }
-        return nums;    
+        reverse(ans.begin(), ans.end());
+        return ans;    
     }
 };
