eadacbbaae60172f4ad63da5f05d6663058053d9
Dyutiman Bharadwaj <148200116+DYUTIMAN03@users.noreply.github.com>
2026-09-13 15:08:04 +0530

    Time: 4 ms (67.63%) | Memory: 134.3 MB (84.68%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
@@ -1,33 +1,77 @@
+// class Solution {
+// public:
+//     vector<int> getAverages(vector<int>& nums, int k) {
+//         int n = nums.size();
+
+//         if(k==0){
+//             return nums;
+//         }
+
+//         vector<int> result(n,-1);
+
+//         if(n < 2*k+1){
+//             return result;
+//         }
+
+//         vector<long long> prefixSum(n,0);
+//         prefixSum[0] = nums[0];
+
+//         for(int i=1; i<n; i++){
+//             prefixSum[i] = prefixSum[i-1] + nums[i];
+//         }
+
+//         for(int i=k; i <= n-k-1; i++){
+//             int left_idx = i-k;
+//             int right_idx = i+k;
+
+//             long long sum = prefixSum[right_idx];
+//             if(left_idx > 0) sum -= prefixSum[left_idx - 1];
+
+//             result[i] = sum/(2*k+1);
+//         }
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
         int n = nums.size();
 
-        if(k==0){
+        if (k == 0)
             return nums;
-        }
 
-        vector<int> result(n,-1);
+        vector<int> result(n, -1);
 
-        if(n < 2*k+1){
+        if (n < 2 * k + 1)
             return result;
+
+        long long window = 0;
+
+        int left = 0;
+        int right = 2 * k;
+        int i = k;
+
+        for (int j = left; j <= right; j++) {
+            window += nums[j];
         }
 
-        vector<long long> prefixSum(n,0);
-        prefixSum[0] = nums[0];
+        result[i] = window / (2 * k + 1);
 
-        for(int i=1; i<n; i++){
-            prefixSum[i] = prefixSum[i-1] + nums[i];
-        }
+        i++;
+        right++;
 
-        for(int i=k; i <= n-k-1; i++){
-            int left_idx = i-k;
-            int right_idx = i+k;
+        while (right < n) {
+            int out_of_window = nums[left];
+            int came_to_window = nums[right];
 
-            long long sum = prefixSum[right_idx];
-            if(left_idx > 0) sum -= prefixSum[left_idx - 1];
+            window = window + came_to_window - out_of_window;
 
-            result[i] = sum/(2*k+1);
+            result[i] = window / (2 * k + 1);
+
+            i++;
+            right++;
+            left++;
         }
         return result;
     }
